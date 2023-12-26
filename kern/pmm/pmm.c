aeb943da797b7024282ba5d169c2d173a50dd408
kurkii <kurkiie@protonmail.com>
2023-12-26 20:48:15 +0100

    finished pmm

--- kern/pmm/pmm.c
+++ kern/pmm/pmm.c
@@ -27,9 +27,6 @@ uint8_t *bitmap             = 0;        // we are doing a bitmap allocator
 struct limine_memmap_response *memmap;
 extern uint64_t hhdmoffset;
 
-
-
-
 void mmap_set (int bit) {
     bitmap[bit/8] |= (1 << (bit%8));
 }
@@ -37,6 +34,11 @@ void mmap_set (int bit) {
 bool mmap_test (int bit) {
     return bitmap[bit/8] &  (1 << (bit%8));
 }
+
+void mmap_unset (int bit) {
+    bitmap[bit / 8] &= ~ (1 << (bit % 8));
+}
+
 
 uint64_t get_block_count(){
     return mem_size;
@@ -54,34 +56,26 @@ uint64_t bit2block(uint64_t bit){
     return (bit/8) / BLOCK_SIZE;
 }
 
-uint64_t block2bit(uint64_t block){
-    return block * 8;
-}
 int find_free_block(void){
-	//! find the first free bit
-	for (uint8_t i = 0; i < get_block_count() / 8; i++)
-		if (bitmap[i] != 0xff)
-			for (int j = 0; j < 8; j++) {		//! test each bit in the byte
-				int bit = 1 << j;
-				if (!(bitmap[i] & bit) )
-					return i*8+j;
+	for (uint8_t i = 0; i < get_block_count() / 8; i++) // find first free block
+		if (bitmap[i] != 0xff)                          // if it isnt all set
+			for (int j = 0; j < 8; j++) {		        // loop through the uint8
+				int bit = 1 << j;                       // move 1 to specific bit
+				if (!(bitmap[i] & bit) )                // compare bit and bitmap bit
+					return i*8+j;                       // i = bit, so bit*8 = byte in memory +j i dont fully get but whatever
 			}
  
 	return -1;
 }
 
-void init_bitmap(){
-    
-    struct limine_memmap_entry **entries = memmap->entries;
-    for(uint64_t i = 0; i < memmap_entry_count; i++){
-        switch (entries[i]->type) {
-            case !(LIMINE_MEMMAP_USABLE)||!(LIMINE_MEMMAP_BOOTLOADER_RECLAIMABLE):
-                for(uint64_t j = 0; j < entries[i]->length; j += BLOCK_SIZE){
-                    mmap_set(addr2block((entries[i]->base+j) / BLOCK_SIZE));
-                }
+void get_mem_size(struct limine_memmap_entry **entries){
+    for(int i = 0; i < memmap_entry_count; i++){
+        switch(entries[i]->type){
+            case LIMINE_MEMMAP_USABLE:
+                mem_size += entries[i]->length;
+                break;
         }
     }
-
 }
 
 void parse_memmap(struct limine_memmap_entry **entries){
@@ -95,7 +89,7 @@ void parse_memmap(struct limine_memmap_entry **entries){
                     usable_found = 1;
                 }
                 printf("pmm: usable: 0x{x}, length: 0x{xn}", entries[i]->base, entries[i]->length);
-                mem_size += entries[i]->length;
+                
                 break;
             default:
                 printf("pmm: type={d}, base=0x{x}, length=0x{xn}", entries[i]->type, entries[i]->base, entries[i]->length);
@@ -109,7 +103,6 @@ void parse_memmap(struct limine_memmap_entry **entries){
     }
 }
 
-
 void pmm_init(){
 
     if(memmap_request.response == NULL){
@@ -120,25 +113,27 @@ void pmm_init(){
     struct limine_memmap_entry **entries = memmap->entries;
     memmap_entry_count = memmap->entry_count;
 
-    
-    parse_memmap(entries);
+    get_mem_size(entries);      // writes size of memory into mem_size 
+    parse_memmap(entries);      
 
     free_blocks = (usable_top / BLOCK_SIZE) * 8;
     block_count = mem_size/8;
-    printf("memsize: {d}MB{n}", mem_size/1000000);
-    printf("block count: {d}KB{n}", (block_count)/1000);
+
+    printf("pmm: memsize: {d}MB{n}", mem_size/1000000);
+    printf("pmm: block count: {d}KB{n}", (block_count)/1000);
+    printf("pmm: size of bitmap: {d}KB{n}", (block_count/8)/1000);
 
     printf("pmm: placing bitmap at 0x{xn}", usable_addr);
     bitmap = (uint64_t*)(usable_addr+hhdmoffset);
+    memset(bitmap, 0xFF, block_count/8);
 
-    
-    //memset(bitmap, 0x90, );
-    init_bitmap();
-
-    printf("{dn}", find_free_block());
-    printf("0x{xn}", find_free_block());
-
-
-
+    for(uint64_t i = 0; i < memmap_entry_count; i++){   // set usable blocks in the bitmap
+        switch (entries[i]->type) {
+            case (LIMINE_MEMMAP_USABLE):
+                for(uint64_t j = 0; j < entries[i]->length; j += BLOCK_SIZE){
+                    mmap_unset(addr2block((entries[i]->base+j) / BLOCK_SIZE));
+                }
+        }
+    }
 
 }
